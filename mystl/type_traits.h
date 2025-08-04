ee695bd50cb5b352b6719d0be1b075f986e73010
random name <60777462+jjjllxx@users.noreply.github.com>
2025-08-04 10:32:19 +0800

    type_traits: add enable_if, is_integral and is_same

--- mystl/type_traits.h
+++ mystl/type_traits.h
@@ -36,4 +36,107 @@ struct remove_reference<T&&>
 {
     using Type = T;
 };
+
+template<class T>
+struct enable_if<true, T>
+{
+    using Type = T;
+};
+
+template<bool B, typename T = void>
+using enable_if_t = typename enable_if<B, T>::Type;
+
+template<typename T>
+struct is_integral_base : false_type
+{
+};
+
+template<>
+struct is_integral_base<bool> : true_type
+{
+};
+
+template<>
+struct is_integral_base<char> : true_type
+{
+};
+
+template<>
+struct is_integral_base<signed char> : true_type
+{
+};
+
+template<>
+struct is_integral_base<unsigned char> : true_type
+{
+};
+
+template<>
+struct is_integral_base<wchar_t> : true_type
+{
+};
+
+template<>
+struct is_integral_base<short> : true_type
+{
+};
+
+template<>
+struct is_integral_base<unsigned short> : true_type
+{
+};
+
+template<>
+struct is_integral_base<int> : true_type
+{
+};
+
+template<>
+struct is_integral_base<unsigned int> : true_type
+{
+};
+
+template<>
+struct is_integral_base<long> : true_type
+{
+};
+
+template<>
+struct is_integral_base<unsigned long> : true_type
+{
+};
+
+template<>
+struct is_integral_base<long long> : true_type
+{
+};
+
+template<>
+struct is_integral_base<unsigned long long> : true_type
+{
+};
+
+template<typename T>
+struct is_integral : is_integral_base<typename remove_reference<T>::Type>
+{
+};
+
+template<typename T>
+using is_integral_t = typename is_integral<T>::Type;
+
+template<typename T>
+constexpr bool is_integral_v = is_integral<T>::Value;
+
+template<class T, class U>
+struct is_same : mystl::false_type
+{
+};
+
+template<class T>
+struct is_same<T, T> : mystl::true_type
+{
+};
+
+template<class T, class U>
+constexpr bool is_same_v = is_same<T, U>::Value;
 } // namespace mystl
