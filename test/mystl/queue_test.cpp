6324c49306116541cb6e0ddc4241a17b6bced04f
random name <60777462+jjjllxx@users.noreply.github.com>
2025-09-02 18:03:07 +0800

    queue_test: integrate with mystl

--- test/mystl/queue_test.cpp
+++ test/mystl/queue_test.cpp
@@ -1,121 +1,67 @@
 #include "mystl/queue.h"
 #include <gtest/gtest.h>
-#include <stdexcept>
 
-GTEST_TEST(DS_Queue, q_empty_true)
+GTEST_TEST(mystl_queue, q_empty_true)
 {
     mystl::queue<int> q;
 
     EXPECT_TRUE(q.empty());
 }
 
-GTEST_TEST(DS_Queue, q_empty_false)
+GTEST_TEST(mystl_queue, q_empty_false)
 {
     mystl::queue<int> q;
-    q.enqueue(2);
+    q.push(2);
 
     EXPECT_FALSE(q.empty());
 }
 
-GTEST_TEST(DS_Queue, q_enqueue)
+GTEST_TEST(mystl_queue, q_push)
 {
     mystl::queue<int> q;
-    q.enqueue(2);
-
-    EXPECT_EQ(q.dequeue(),2);
+    q.push(2);
+    EXPECT_EQ(q.front(), 2);
 }
 
-GTEST_TEST(DS_Queue, q_dequeue)
+GTEST_TEST(mystl_queue, front)
 {
     mystl::queue<int> q;
-    q.enqueue(2);
+    q.push(2);
+    q.push(5);
+    q.push(3);
+    q.push(4);
 
-    EXPECT_EQ(q.dequeue(), 2);
+    EXPECT_EQ(q.front(), 2);
+}
+
+GTEST_TEST(mystl_queue, back)
+{
+    mystl::queue<int> q;
+    q.push(2);
+    q.push(5);
+    q.push(3);
+    q.push(4);
+
+    EXPECT_EQ(q.back(), 4);
+}
+
+GTEST_TEST(mystl_queue, q_pop)
+{
+    mystl::queue<int> q;
+    q.push(2);
+
+    EXPECT_EQ(q.front(), 2);
+    q.pop();
     EXPECT_TRUE(q.empty());
 }
 
-GTEST_TEST(DS_Queue, q_dequeue_out_of_range)
-{
-    mystl::queue<int> q;
-
-    EXPECT_THROW(q.dequeue(), std::out_of_range);
-}
-
-GTEST_TEST(DS_Queue, q_size)
+GTEST_TEST(mystl_queue, q_size)
 {
     mystl::queue<int> q;
     EXPECT_EQ(q.size(), 0);
-    q.enqueue(2);
-    q.enqueue(5);
-    q.enqueue(3);
-    q.enqueue(4);
+    q.push(2);
+    q.push(5);
+    q.push(3);
+    q.push(4);
     EXPECT_EQ(q.size(), 4);
-}
-
-GTEST_TEST(DS_Queue, fsq_empty_true)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-
-    EXPECT_TRUE(fsq.empty());
-}
-
-GTEST_TEST(DS_Queue, fsq_empty_false)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-    fsq.enqueue(2);
-
-    EXPECT_FALSE(fsq.empty());
-}
-
-GTEST_TEST(DS_Queue, fsq_full_true)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-    fsq.enqueue(2);
-    fsq.enqueue(5);
-    fsq.enqueue(3);
-    fsq.enqueue(4);
-
-    EXPECT_TRUE(fsq.full());
-}
-
-GTEST_TEST(DS_Queue, fsq_full_false)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-    fsq.enqueue(2);
-
-    EXPECT_FALSE(fsq.full());
-}
-
-GTEST_TEST(DS_Queue, fsq_enqueue)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-    fsq.enqueue(2);
-
-    EXPECT_EQ(fsq.dequeue(), 2);
-}
-
-GTEST_TEST(DS_Queue, fsq_enqueue_overflow)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-    fsq.enqueue(2);
-    fsq.enqueue(5);
-    fsq.enqueue(3);
-    fsq.enqueue(4);
-    EXPECT_THROW(fsq.enqueue(5), std::overflow_error);
-}
-
-GTEST_TEST(DS_Queue, fsq_dequeue)
-{
-    mystl::FixedSizeQueue<int> fsq { 4 };
-    fsq.enqueue(2);
-
-    EXPECT_EQ(fsq.dequeue(), 2);
-    EXPECT_TRUE(fsq.empty());
-}
-
-GTEST_TEST(DS_Queue, fsq_dequeue_out_of_range)
-{
-    mystl::FixedSizeQueue<int> q { 4 };
-
-    EXPECT_THROW(q.dequeue(), std::out_of_range);
 }
